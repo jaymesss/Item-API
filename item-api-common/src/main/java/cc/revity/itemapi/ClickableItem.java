73d6aab9e128fab35d5319b4ca2b2911638b0fda
jaymesss <hoffmannjamesjr@gmail.com>
2021-03-23 22:49:12 -0700

    Create ClickableItem javadocs

--- item-api-common/src/main/java/cc/revity/itemapi/ClickableItem.java
+++ item-api-common/src/main/java/cc/revity/itemapi/ClickableItem.java
@@ -4,16 +4,21 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.block.Action;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.List;
-
 public abstract class ClickableItem {
 
+    /**
+     * @return the id of the {@link ClickableItem}
+     */
     public abstract String getId();
 
-    public abstract String getItemName();
+    /**
+     * @return the {@link ItemStack} of the {@link ClickableItem}
+     */
+    public abstract ItemStack getItemStack();
 
-    public abstract List<String> getLore();
-
+    /**
+     * @return the clicking logic of the {@link ClickableItem}
+     */
     public abstract boolean onClick(Player player, ItemStack clicked, Action action);
 
 }
